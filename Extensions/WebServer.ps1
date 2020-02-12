8434da7e770afc43fe9285cc4b535744b50b4ab7
gitName <a@b.c>
2020-02-12 16:50:02 +0100

    Version 1.2 - WPI añadido a la Extension

--- Extensions/WebServer.ps1
+++ Extensions/WebServer.ps1
@@ -17,11 +17,21 @@ Configuration WebServer
       Ensure = "Present"
       Name = "Web-Asp-Net45"
     }
-
-     WindowsFeature WebServerManagementConsole
+    #Install IIS Management Console
+    WindowsFeature WebServerManagementConsole
     {
-        Name = "Web-Mgmt-Console"
-        Ensure = "Present"
+      Name = "Web-Mgmt-Console"
+      Ensure = "Present"
     }
+ 		#Install Web Platform Installer
+		Package WebPi_Installation
+		{
+			Ensure = "Present"
+			Name = "Microsoft Web Platform Installer 5.0"
+			Path = Join-Path $PSScriptRoot wpilauncher.exe
+			ProductId = '4D84C195-86F0-4B34-8FDE-4A17EB41306A'
+			Arguments = ''
+		}
+
   }
 } 
